99f680f69cbbb6164b71fd3a36661cfc3da28a44
isaackogan <isaacikogan@gmail.com>
2024-12-21 17:43:40 -0500

    Bump `unstructured_expanded==0.16.11.post2`

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
             "bs4",
             "pandas",
             "unstructured[all-docs]==0.16.11",
-            "unstructured_expanded==0.16.11.post1",  # should have been .4 but i messed up. remember in future to keep parity with unstructured
+            "unstructured_expanded==0.16.11.post2",  # should have been .4 but i messed up. remember in future to keep parity with unstructured
             "numpy==1.26.4"
         ],
         classifiers=[
