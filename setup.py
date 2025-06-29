6a3b3647e666b537eda3360342938234d7dcc218
isaackogan <isaacikogan@gmail.com>
2025-06-29 12:55:08 -0400

    Markdown support

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ manifest: dict = {
     "name": "SemanticDocumentParser",
     "license": "MIT",
     "author": "Isaac Kogan",
-    "version": "0.1.6",
+    "version": "0.1.7",
     "email": "info@isaackogan.com"
 }
 
